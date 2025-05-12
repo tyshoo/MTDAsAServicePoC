6fbfa4bdad850313f9c5017bd224f2753b7d2561
tyshoo <54097381+tyshoo@users.noreply.github.com>
2025-05-13 01:20:32 +0800

    added ingestion module

--- infra/main.tf
+++ infra/main.tf
@@ -3,3 +3,11 @@ module "agent_mgmt" {
   environment       = var.environment
   agent_bucket_name = var.agent_bucket_name   # USER INPUT: define this in infra/variables.tf
 }
+
+module "ingestion" {
+  source             = "../modules/ingestion"
+  environment        = var.environment
+  lambda_function_name = var.lambda_function_name   # from infra/variables.tf
+  api_name           = var.api_name                 # from infra/variables.tf
+  stage_name         = var.stage_name               # typically "poc"
+}
