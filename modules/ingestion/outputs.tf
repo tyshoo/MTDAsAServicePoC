7015c9bc8520e434265dd7ad1a8c1e0a78043cc7
tyshoo <54097381+tyshoo@users.noreply.github.com>
2025-05-25 13:47:23 +0800

    Update outputs.tf
    added sns and waf

--- modules/ingestion/outputs.tf
+++ modules/ingestion/outputs.tf
@@ -1,9 +1,19 @@
 output "lambda_function_name" {
-  description = "Name of ingestion Lambda"
+  description = "Name of the ingestion Lambda"
   value       = aws_lambda_function.ingest.function_name
 }
 
 output "api_invoke_url" {
-  description = "Invocation URL for the POST /events endpoint"
-  value       = "${aws_api_gateway_deployment.deployment.invoke_url}${var.stage_name}/events"
+  description = "URL for POST /events"
+  value       = "${aws_api_gateway_stage.stage.invoke_url}${var.stage_name}/events"
+}
+
+output "sns_alerts_topic_arn" {
+  description = "SNS topic ARN for alarms"
+  value       = aws_sns_topic.alerts.arn
+}
+
+output "waf_associated" {
+  description = "Whether a WAF WebACL was associated"
+  value       = var.web_acl_arn != ""
 }
