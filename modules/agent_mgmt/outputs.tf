e7bbeb1111ef909542f12972682543d3dd93b0b7
tyshoo <54097381+tyshoo@users.noreply.github.com>
2025-05-25 13:29:17 +0800

    Update outputs.tf
    cw alarms for s3 object delete

--- modules/agent_mgmt/outputs.tf
+++ modules/agent_mgmt/outputs.tf
@@ -8,3 +8,8 @@ output "agent_download_url" {
   # USER ACTION: You may want to generate a presigned URL or configure a bucket policy
   value = "s3://${aws_s3_bucket.agent_bucket.id}/${var.environment}/agent_v0.1.0.bin"
 }
+
+output "s3_delete_alarm_name" {
+  description = "Name of CloudWatch Alarm on S3 deletes"
+  value       = aws_cloudwatch_metric_alarm.s3_delete_alarm.alarm_name
+}
