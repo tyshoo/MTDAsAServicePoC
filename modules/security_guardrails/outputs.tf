a99ff4b6231790193c86951815fa3c91d13741ca
tyshoo <54097381+tyshoo@users.noreply.github.com>
2025-05-25 18:55:18 +0800

    Update outputs.tf
    added sns, compliance alarm

--- modules/security_guardrails/outputs.tf
+++ modules/security_guardrails/outputs.tf
@@ -12,3 +12,13 @@ output "cloudtrail_bucket" {
   description = "S3 bucket storing CloudTrail logs"
   value       = aws_s3_bucket.trail_bucket.id
 }
+
+output "security_alerts_sns" {
+  description = "SNS topic ARN for security alerts"
+  value       = aws_sns_topic.alerts.arn
+}
+
+output "config_noncompliant_alarm" {
+  description = "CloudWatch Alarm for Config non‑compliance"
+  value       = aws_cloudwatch_metric_alarm.config_noncompliant.alarm_name
+}
